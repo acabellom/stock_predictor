cee5c505854bd43dd96751e49bc894f891126933
acabellom <acabellom01@gmail.com>
2026-01-24 22:45:30 +0100

    feat: add tests for fetch 2 years data

--- tests/test_data_collector.py
+++ tests/test_data_collector.py
@@ -1,8 +1,8 @@
 import pytest
 from unittest.mock import patch, Mock
-from stock_predictor.data_collector import fetch_stock_data_month
+from stock_predictor.data_collector import fetch_stock_data_month, fetch_last_2_years
 import requests
-
+from datetime import datetime
 
 @patch("stock_predictor.data_collector.requests.get")
 def test_fetch_stock_data_month_success(mock_get):
@@ -88,3 +88,58 @@ def test_fetch_stock_data_month_leap_year(mock_get):
 
     called_url = mock_get.call_args[0][0]
     assert "2024-02-29" in called_url
+
+
+@patch("stock_predictor.data_collector.sleep")
+@patch("stock_predictor.data_collector.fetch_last_month_")
+@patch("stock_predictor.data_collector.fetch_stock_data_month")
+@patch("stock_predictor.data_collector.datetime")
+def test_fetch_last_2_years_ok(
+    mock_datetime, mock_fetch_month, mock_fetch_last_month, mock_sleep
+):
+    """
+    Test fetching historical stock data for the last 2 years successfully.
+    :param mock_datetime: Mocked datetime module.
+    :param mock_fetch_month: Mocked fetch_stock_data_month function.
+    :param mock_fetch_last_month: Mocked fetch_last_month_ function.
+    :param mock_sleep: Mocked sleep function.
+    """
+
+    mock_datetime.today.return_value = datetime(2024, 1, 15)
+    mock_datetime.side_effect = lambda *args, **kwargs: datetime(*args, **kwargs)
+
+    mock_fetch_month.return_value = {"results": [{"price": 100}, {"price": 101}]}
+
+    mock_fetch_last_month.return_value = {"results": [{"price": 200}]}
+
+    data = fetch_last_2_years("AAPL")
+
+    assert isinstance(data, list)
+    assert len(data) > 0
+    assert {"price": 100} in data
+    assert {"price": 200} in data
+
+    assert mock_fetch_month.called
+    assert mock_fetch_last_month.called
+
+    mock_sleep.assert_called()
+
+
+@patch("stock_predictor.data_collector.sleep")
+@patch("stock_predictor.data_collector.fetch_last_month_")
+@patch("stock_predictor.data_collector.fetch_stock_data_month")
+def test_fetch_last_2_years_handles_exception(
+    mock_fetch_month, mock_fetch_last_month, mock_sleep
+):
+    """
+    Test that fetch_last_2_years handles exceptions during monthly data fetching.
+     :param mock_fetch_month: Mocked fetch_stock_data_month function.
+     :param mock_fetch_last_month: Mocked fetch_last_month_ function.
+     :param mock_sleep: Mocked sleep function.
+    """
+    mock_fetch_month.side_effect = Exception("API down")
+    mock_fetch_last_month.return_value = {"results": []}
+
+    data = fetch_last_2_years("AAPL")
+
+    assert data == []
