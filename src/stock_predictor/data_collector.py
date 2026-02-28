d3205bc05ac58aa445b5d64a62977ead816bb6fe
acabellom <acabellom01@gmail.com>
2026-02-28 19:42:15 +0100

    format: file reformated to a better style

--- src/stock_predictor/data_collector.py
+++ src/stock_predictor/data_collector.py
@@ -213,10 +213,11 @@ def get_lowest_date_in_s3(ticker: str, s3_client) -> str:
     response = s3_client.list_objects_v2(Bucket=bucket_name)
     content = response.get("Contents", [])
     if content == []:
-        logger.info("No files found in S3 bucket. Returning today 2 year ago as lowest date.")
+        logger.info(
+            "No files found in S3 bucket. Returning today 2 year ago as lowest date."
+        )
         return (datetime.now() - timedelta(days=730)).strftime("%Y%m%d")
     for obj in content:
-
         file_key = obj["Key"]
         if file_key.startswith("raw_"):
             parts = file_key.split("_")
@@ -248,6 +249,7 @@ def store_old_data(s3_client, ticker: str, old_file_key: str):
     s3_client.delete_object(Bucket=bucket_name, Key=old_file_key)
     logger.info(f"Archived {old_file_key} to {archive_key}.")
 
+
 def check_highest_date_in_s3(ticker: str, s3_client) -> str:
     """
     Get the highest date from the raw data files stored in S3 for a given ticker.
@@ -263,7 +265,9 @@ def check_highest_date_in_s3(ticker: str, s3_client) -> str:
     response = s3_client.list_objects_v2(Bucket=bucket_name)
     content = response.get("Contents", [])
     if content == []:
-        logger.info("No files found in S3 bucket. Returning '00000000' as highest date.")
+        logger.info(
+            "No files found in S3 bucket. Returning '00000000' as highest date."
+        )
         return "00000000"
     for obj in response.get("Contents", []):
         file_key = obj["Key"]
@@ -271,6 +275,7 @@ def check_highest_date_in_s3(ticker: str, s3_client) -> str:
             parts = file_key.split("_")
     logger.info(f"Highest date found: {parts[2].split('.')[0]}")
     return parts[2].split(".")[0]
+
 
 def create_bucket_if_not_exists(s3_client, bucket_name: str):
     """
@@ -289,6 +294,7 @@ def create_bucket_if_not_exists(s3_client, bucket_name: str):
     else:
         logger.info(f"Bucket {bucket_name} already exists.")
 
+
 def main():
     ticker = "TSLA"
     logger.info(f"Starting data collection for {ticker}...")
@@ -299,7 +305,7 @@ def main():
         logger.info(f"Fetched {len(raw_data)} data points for {ticker}.")
         logger.info("Processing stock data...")
         processed_data = process_stock_data(raw_data)
-        
+
         final_data, old_file_key = merge_raw_data(ticker, s3_client, processed_data)
         final_data.to_csv(f"data/{ticker}_historical_data.csv")
         logger.info(f"Processed and saved historical data for {ticker}.")
@@ -311,7 +317,9 @@ def main():
         )
         store_old_data(s3_client, ticker, old_file_key)
     else:
-        logger.info(f"Data for {ticker} is already up to date in S3. No new data fetched.")
+        logger.info(
+            f"Data for {ticker} is already up to date in S3. No new data fetched."
+        )
 
 
 if __name__ == "__main__":
