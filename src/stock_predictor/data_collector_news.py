1084def3efe6bb13576cf2c038ac3fe7d875fa6a
acabellom <acabellom01@gmail.com>
2026-03-08 11:47:38 +0100

    fix: now function retrieves data for last 2 years

--- src/stock_predictor/data_collector_news.py
+++ src/stock_predictor/data_collector_news.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from datetime import datetime
 import requests
+import pandas as pd
 
 load_dotenv()
 
@@ -21,10 +22,20 @@ def fetch_news_data(ticker: str, date: datetime) -> dict:
     date_2_years_ago = date.replace(year=date.year - 2)
     date_str = date.strftime("%Y-%m-%d")
     date_str_2_years_ago = date_2_years_ago.strftime("%Y-%m-%d")
-    url = f"https://api.polygon.io/v2/reference/news?ticker={ticker}&published_utc.gte={date_str_2_years_ago}T00:00:00Z&published_utc.lte={date_str}T23:59:59Z&apiKey={API_KEY}"
+    url = f"https://api.polygon.io/v2/reference/news?ticker={ticker}&published_utc.gte={date_str_2_years_ago}T00:00:00Z&published_utc.lte={date_str}T23:59:59Z&apiKey={API_KEY}&limit=1000"
     response = requests.get(url)
     response.raise_for_status()
-    return response.json()
+    data = response.json()
+    all_results = data.get("results", [])
+    next_url = data.get("next_url")
+    while next_url:
+        next_url = next_url + f"&apiKey={API_KEY}"
+        next_response = requests.get(next_url)
+        next_response.raise_for_status()
+        all_results.extend(next_response.json().get("results", []))
+        next_url = next_response.json().get("next_url")
+    data["results"] = all_results
+    return data
 
 
 def extract_headlines(news_data: dict) -> list:
@@ -48,4 +59,7 @@ def extract_headlines(news_data: dict) -> list:
 
 if __name__ == "__main__":
     news_data = fetch_news_data("AAPL", datetime.now())
-    print(extract_headlines(news_data))
+    df = extract_headlines(news_data)
+    df = pd.DataFrame(df, columns=["headline", "published_utc"])
+
+    df.to_csv("./data/aapl_news_test.csv", index=False, quoting=1)
