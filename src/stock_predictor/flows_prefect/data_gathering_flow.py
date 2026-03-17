45ea34cb9509600851173600a05d3c056e970617
acabellom <acabellom01@gmail.com>
2026-03-17 09:14:49 +0100

    feat: add full prefect flow of data gathering

--- src/stock_predictor/flows_prefect/data_gathering_flow.py
+++ src/stock_predictor/flows_prefect/data_gathering_flow.py
@@ -13,7 +13,51 @@ from stock_predictor.utils import (
     upload_to_s3,
     get_lowest_date_in_s3,
 )
+from stock_predictor.data_collector_news import (
+    fetch_news_data,
+    extract_headlines,
+    clean_data,
+    get_dataframe,
+    get_sentiment_analysis,
+    merge_prices_news,
+    download_model_locally,
+)
 from datetime import datetime
+
+
+@task
+def fetch_news_data_task(ticker, date):
+    return fetch_news_data(ticker, date)
+
+
+@task
+def extract_headlines_task(news_data):
+    return extract_headlines(news_data)
+
+
+@task
+def clean_data_task(df):
+    return clean_data(df)
+
+
+@task
+def get_dataframe_task(df):
+    return get_dataframe(df)
+
+
+@task
+def get_sentiment_analysis_task(df):
+    return get_sentiment_analysis(df)
+
+
+@task
+def merge_prices_news_task(df, df_prices):
+    return merge_prices_news(df, df_prices)
+
+
+@task
+def download_model_locally_task():
+    download_model_locally()
 
 
 @task
@@ -74,11 +118,15 @@ def data_gathering_flow(ticker: str):
     s3_client = create_s3_client_task()
     create_bucket_task(s3_client, ticker)
     if check_data_up_to_date_task(ticker, s3_client):
-        processed_data = fetch_and_process_data_task(ticker)
-
-        final_data, old_file_key = merge_raw_data_task(
-            ticker, s3_client, processed_data
-        )
+        download_model_locally_task.submit()
+        processed_data_price = fetch_and_process_data_task.submit(ticker)
+        processed_data_news = fetch_news_data_task.submit(ticker, datetime.now())
+        df_headlines = extract_headlines_task(processed_data_news)
+        df_cleaned = clean_data_task(df_headlines)
+        df_dataframe = get_dataframe_task(df_cleaned)
+        df_sentiment = get_sentiment_analysis_task(df_dataframe)
+        df_merged = merge_prices_news_task(df_sentiment, processed_data_price.result())
+        final_data, old_file_key = merge_raw_data_task(ticker, s3_client, df_merged)
         logger.info(f"Processed and saved historical data for {ticker}.")
         upload_to_s3_task(s3_client, ticker, final_data)
         store_old_data_task(s3_client, ticker, old_file_key)
